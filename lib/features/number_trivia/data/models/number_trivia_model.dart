95250e50f9d8d09c82c37644bb301bbfa0ab4dbd
miggelucas <lucasmigge@gmail.com>
2024-05-09 12:54:41 -0300

    feat: NumberTriviaModel dataLayer

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -2,4 +2,18 @@ import 'package:numbers_trivia/features/number_trivia/domain/entities/number_tri
 
 class NumberTriviaModel extends NumberTrivia {
   const NumberTriviaModel({required super.text, required super.number});
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'] as String,
+      number: (json['number'] as num).toInt(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': this.text,
+      'number': this.number,
+    };
+  }
 }
--- test/features/number_trivia/data/numbert_trivia_model_test.dart
+++ test/features/number_trivia/data/numbert_trivia_model_test.dart
@@ -1,11 +1,39 @@
+import "dart:convert";
+
 import "package:flutter_test/flutter_test.dart";
 import "package:numbers_trivia/features/number_trivia/data/models/number_trivia_model.dart";
 import "package:numbers_trivia/features/number_trivia/domain/entities/number_trivia.dart";
 
+import "../../../fixtures/fixture_reader.dart";
+
 void main() {
-  const tNumberTriviaModel = NumberTriviaModel(number: 1, text: "Test Text");
+  const tNumberTriviaModel = NumberTriviaModel(number: 1, text: "test text");
 
   test('should be a subcasst of NumberTrivia', () async {
     expect(tNumberTriviaModel, isA<NumberTrivia>());
+  });
+
+  group('fromJson', () {
+    test('should return a valid model when the JSON number is an integer',
+        () async {
+      final Map<String, dynamic> jsonMap = json.decode(fixture("trivia.json"));
+
+      final result = NumberTriviaModel.fromJson(jsonMap);
+
+      expect(result, tNumberTriviaModel);
+    });
+  });
+
+  group('toJson', () {
+    test('should return a JSON map containing the proper data', () async {
+      final result = tNumberTriviaModel.toJson();
+
+      final expectedMap = {
+        "text": "test text",
+        "number": 1,
+      };
+
+      expect(result, expectedMap);
+    });
   });
 }
